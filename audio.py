1611c1b11fe94748aa971780e469909129f282e6
Nia Watts <niawatts95@gmail.com>
2017-11-18 18:38:50 -0700

    Added Comments and Cleaned Logic

--- audio.py
+++ audio.py
@@ -4,35 +4,38 @@ import pyaudio
 import numpy as np
 
 class Audio:
+    # init function
     def __init__(self, audioFile='audio/SmashMouth-AllStar.wav'):
         self.audioFile = audioFile
 
-        #define stream chunk
+        # define stream chunk
         self.chunk = 1024
 
-        #define pyaudio
+        # define pyaudio
         self.p = pyaudio.PyAudio()
         self.song = wave.open(self.audioFile, 'rb')
         self.samprate, self.wavdata = scipy.io.wavfile.read(self.audioFile)
 
-        #get stream
+        # get stream
         self.stream = self.p.open(
             format = self.p.get_format_from_width(self.song.getsampwidth()),
             channels = self.song.getnchannels(),
             rate = self.song.getframerate(),
             output = True)
 
-        #read data
+        # read data
         self.data = self.song.readframes(self.chunk)
-        self.frameLocation = 0
 
+        # setup initial data
+        self.frameLocation = 0
+        self.levels = 0
+
+    # plays the full song
     def playFull(self):
         try:
             while self.data:
-                self.stream.write(self.data)
-                self.data = self.song.readframes(self.chunk)
-                print(np.average(self.wavdata[self.frameLocation]))
-                self.frameLocation += self.chunk
+                # play frame
+                self.playFrame()
 
         finally:
             #stop stream
@@ -42,14 +45,21 @@ class Audio:
             #close PyAudio
             self.p.terminate()
 
+    # plays an individual frame
     def playFrame(self):
+        # handle stream
         self.stream.write(self.data)
         self.data = self.song.readframes(self.chunk)
-        print(np.average(self.wavdata[self.frameLocation]))
+
+        # get levels
+        self.levels = np.average(self.wavdata[self.frameLocation])
+
+        # advance frames
         self.frameLocation += self.chunk
 
+    # closes the object
     def close(self):
-        #stop stream
+        # stop stream
         self.stream.stop_stream()
         self.stream.close()
 
--- audio.test.py
+++ audio.test.py
@@ -2,7 +2,13 @@ import audio
 import time
 
 audioTestObject = audio.Audio('audio/lifeOnMars.wav')
+
+# this works, but it opens a while loop, so you can't interact much.
 # audioTestObject.playFull()
 
+# this way allows you to interact with the object and do other stuff, such as printing levels.
 while audioTestObject.data:
     audioTestObject.playFrame()
+    print(audioTestObject.levels)
+
+audioTestObject.close()
