15a067464bdc110468aa6e44de59c889ffb712b0
Nia Watts <niawatts95@gmail.com>
2017-11-18 18:45:47 -0700

    cleaned up comments

--- audio.test.py
+++ audio.test.py
@@ -3,10 +3,10 @@ import time
 
 audioTestObject = audio.Audio('audio/lifeOnMars.wav')
 
-# this works, but it opens a while loop, so you can't interact much.
+# this works, but it opens a while loop, so you can't interact much
 # audioTestObject.playFull()
 
-# this way allows you to interact with the object and do other stuff, such as printing levels.
+# this way allows you to interact with the object and do other stuff, such as printing levels
 while audioTestObject.data:
     audioTestObject.playFrame()
     print(audioTestObject.levels)
