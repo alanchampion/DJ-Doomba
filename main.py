21c0f4db9818653f4d0ed01d5631c53940af6a3f
Nia Watts <niawatts95@gmail.com>
2017-11-18 18:14:46 -0700

    Restored Main.py

--- main.py
+++ main.py
@@ -4,59 +4,17 @@
 #that lights up LEDs based on the decibal level of the sounds it is playing. Attatched
 #to the top is a rotating tentacle that wields a knife to intimidate your enemies.
 
-# Imports
-#import RPi.GPIO as GPIO, time, math
-import wave
-import scipy.io.wavfile
-import pyaudio
-import numpy as np
-# GPIO Junk
-#GPIO.setmode(GPIO.BCM)
+import RPi.GPIO as GPIO, time, math
 
-audioFile = 'audio/SmashMouth-AllStar.wav'
-
-#define stream chunk
-chunk = 1024
-
-#instantiate PyAudio
-p = pyaudio.PyAudio()
-
-song = wave.open(audioFile, 'rb')
-samprate, wavdata = scipy.io.wavfile.read(audioFile)
-#open stream
-stream = p.open(format = p.get_format_from_width(song.getsampwidth()),
-                channels = song.getnchannels(),
-                rate = song.getframerate(),
-                output = True)
-
-#read data
-data = song.readframes(chunk)
-
-frameLocation = 0
+GPIO.setmode(GPIO.BCM)
 
 try:
-    #while True:
+    while True:
         #Buttons to modify sound
-        #print('hi')
-        #print(allStar)
-    #play stream
-    while data:
-        stream.write(data)
-        #print(int.from_bytes(data, 'big'))
-        data = song.readframes(chunk)
-        print(np.average(wavdata[frameLocation]))
-        frameLocation += 1
 
 
 except (KeyboardInterrupt, SystemExit):
     print("Exiting...")
 
-    #stop stream
-    stream.stop_stream()
-    stream.close()
 
-    #close PyAudio
-    p.terminate()
-
-
-#GPIO.cleanup()
+GPIO.cleanup()
