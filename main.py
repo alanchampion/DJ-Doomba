f9da36a635c366d654f9a6ea74976393c7ce34b3
Nia Watts <niawatts95@gmail.com>
2017-12-02 12:05:09 -0700

    Added Pausing

--- main.py
+++ main.py
@@ -6,20 +6,50 @@
 
 import RPi.GPIO as GPIO, time, math
 import audio
+# import gpiozero as gz
 
+# song config
 songs = ['audio/lifeOnMars.wav', 'audio/Short_Skirt_Long_Jacket_by_Cake.wav', 'audio/SmashMouth-AllStar.wav']
-
 songChoice = 0
-
 audioTestObject = audio.Audio(songs[songChoice])
+playing = True
 
+# pin config
+playPauseButton = 27
+
+# GPIO Setup
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(playPauseButton, GPIO.IN)
+# playPauseButton = gz.Button(playPauseButtonPin)
+
+# button setup
+# handles only pressing once by storing previous state
+buttonState = False
+
+def buttonInput(button):
+    return GPIO.input(button) == 1
+
+# handles pausing the song on button press
+def checkPlaying():
+    global buttonState
+    button = buttonInput(playPauseButton)
+    if button == False and button != buttonState:
+        global playing
+        playing = not playing
+
+    buttonState = button
+
 
 try:
     while audioTestObject.data:
-        audioTestObject.playFrame()
-        # print(audioTestObject.calculatedLevelAverage)
-        print(audioTestObject.calculatedLevel)
+        while playing:
+            audioTestObject.playFrame()
+            # print(audioTestObject.calculatedLevelAverage)
+            # print(audioTestObject.calculatedLevel)
+            # print(playing)
+            checkPlaying()
+
+        checkPlaying()
 
     audioTestObject.close()
 
